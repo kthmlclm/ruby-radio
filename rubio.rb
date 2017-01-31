add60e1f0416311494698870d8f36bcc39350446
Lemy Beck <lemybeck@yahoo.co.uk>
2017-01-31 11:27:30 +0000

    Improve now playing

--- rubio.rb
+++ rubio.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 require 'net/http'
 require 'date'
+require 'colorize'
 # check if working directory exists, create it if not
 $work_dir = File.join(Dir.home, ".rubio")
 Dir.mkdir($work_dir) unless File.exists?($work_dir)
@@ -16,52 +17,68 @@ unless File.exists?($work_dir+"/radio_streams")
 end
 $streams = $work_dir+"/radio_streams"
 
-def nownext(arg1,arg2,arg3)
+def nownext(search,now_next,play)
   linenum = 0
   progs = 0
   # get schedule file
   stations = File.open($streams)
   stations.each do |line|
-    linenum = $. if line.downcase.include?('[*') && line.downcase.include?(arg1)
+    linenum = $. if line.downcase.include?('[*') && line.downcase.include?(search)
+    $station = line.scan(/] (.*)/)[0]
     break if linenum != 0
   end
-  schedule_url = URI.parse(URI.encode(stations.each_line.take(2).last.strip))
-  Net::HTTP.start(schedule_url.host, schedule_url.port) do |http|
-    Dir.chdir($work_dir) do
-      schedule = http.get schedule_url
-      # count the number of programmes today
-  #     schedule.body.each_line do |line|
-  #       progs = progs + 1 if line.include?('- is_repeat:')
-  #     end
-      # make an array containing a hash for each programme in schedule
-      $progs = []
-      regex_starts = / start: "(.{20})"/
-      regex_ends = / end: "(.{20})"/
-      regex_synopsis = / short_synopsis: "(.*?)"/
-      regex_title = / display_titles:\s*title: "(.*?)"/
-      regex_subtitle = / subtitle: "(.*?)"/
-      regex_scanner = /(?:#{regex_starts})|(?:#{regex_ends})|(?:#{regex_synopsis})|(?:#{regex_title})|(?:#{regex_subtitle})/m
-      schedule.body.split('- is_repeat').drop(1).each do |programme|
-        match = programme.scan(regex_scanner).flatten.compact
-  #       puts match[1]
-        prog = Hash.new
-        prog['starts'] = match[0]
-        prog['ends'] = match[1]
-        prog['synopsis'] = match[2]
-        prog['title'] = match[3]
-        prog['subtitle'] = match[4]
-        $progs.push prog
+  schedule_url = stations.each_line.take(2).last.strip
+  # if we have a link to a schedule, get the schedule
+  if schedule_url.match(/.*yaml/)
+    schedule_url = URI.parse(URI.encode(schedule_url)) 
+    Net::HTTP.start(schedule_url.host, schedule_url.port) do |http|
+      Dir.chdir($work_dir) do
+        schedule = http.get schedule_url
+#         # count the number of programmes today
+#         schedule.body.each_line do |line|
+#           progs = progs + 1 if line.include?('- is_repeat:')
+#         end
+        # make an array containing a hash for each programme in schedule
+        $progs = []
+        regex_starts = / start: "(.{20})"/
+        regex_ends = / end: "(.{20})"/
+        regex_synopsis = / short_synopsis: "(.*?)"/
+        regex_title = / display_titles:\s*title: "(.*?)"/
+        regex_subtitle = / subtitle: "(.*?)"/
+        regex_scanner = /(?:#{regex_starts})|(?:#{regex_ends})|(?:#{regex_synopsis})|(?:#{regex_title})|(?:#{regex_subtitle})/m
+        schedule.body.split('- is_repeat').drop(1).each do |programme|
+          match = programme.scan(regex_scanner).flatten.compact
+          prog = Hash.new
+          prog['starts'] = match[0]
+          prog['ends'] = match[1]
+          prog['synopsis'] = match[2]
+          prog['title'] = match[3]
+          prog['subtitle'] = match[4]
+          $progs.push prog
+        end
+      
       end
-    
     end
+    # find first instance of end time after now_or_playing
+    $progs.compact.each do |prog|
+      $now_playing = $progs.index( prog )
+      break if ( DateTime.parse(prog['ends']) > DateTime.now )
+    # print now / next info
+    end
+    case play
+    when ARGV[2]
+      line1 = (' Now on' + $station[0]).colorize(:blue)
+    when ''
+      line1 = (' Playing ' + $station[0]).colorize(:blue)
+    end
+    starts = DateTime.parse($progs[$now_playing]['starts']).strftime('%H:%M')
+    ends = DateTime.parse($progs[$now_playing]['ends']).strftime('%H:%M')
+    line2 = ' ' + starts + ' - ' + ends + '  ' + ($progs[$now_playing]['title']).colorize(:yellow)
+    line3 = ('                - ' + $progs[$now_playing]['subtitle']).colorize(:green)
+    puts line1, line2, line3
+  else
+    puts 'no information'
   end 
-  # find first instance of end time after now_or_playing
-  $progs.compact.each do |prog|
-    $now_playing = $progs.index( prog )
-    break if ( DateTime.parse(prog['ends']) > DateTime.now )
-  end
-  
-  puts $progs[$now_playing][arg2]
 
 end
 nownext(ARGV[0],ARGV[1],'')
